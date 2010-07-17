489971c7108cd9dd9c81eda120353776a57f8001
Dmitry Konishchev <konishchev@gmail.com>
2010-07-17 19:21:25 +0400

    Application startup code

--- mlplayer/main.cpp
+++ mlplayer/main.cpp
@@ -18,167 +18,226 @@
 **************************************************************************/
 
 
-#include <QtCore/QTextCodec>
+#include <cstdlib>
+#include <cstdio>
+
+#include <memory>
+
+#include <boost/foreach.hpp>
+
+#include <QtCore/QDir>
+#include <QtCore/QSize>
+#include <QtCore/QTextStream>
 
 #include <QtGui/QApplication>
+#include <QtGui/QIcon>
+
+#include <mlib/gui/core.hpp>
+#include <mlib/sys/system_signal_notifier.hpp>
 
 #include <mlplayer/common.hpp>
 #include <mlplayer/main_window.hpp>
-
 // TODO
 #include <medialib/paths.hpp>
 #include <medialib/subtitles.hpp>
 
 
-int main(int argc, char *argv[])
+namespace {
+
+
+	/// Parses and processes command line options.
+	/// @return paths to video files to play.
+	QStringList	process_command_line_options(const QStringList& args);
+
+
+
+	QStringList process_command_line_options(const QStringList& args)
+	{
+		bool first_arg = true;
+		bool debug = false;
+		bool verbose = false;
+		QStringList videos;
+
+		Q_FOREACH(const QString& arg, args)
+		{
+			if(first_arg)
+			{
+				first_arg = false;
+				continue;
+			}
+
+			if(arg == "--debug")
+				debug = true;
+			else if(arg == "--help")
+			{
+				QTextStream stream(stdout);
+				stream << _F(QApplication::tr(
+						"Usage: %1 [OPTION]...\n"
+						"\n"
+						"Options:\n"
+						"  --help     show this help message\n"
+						"  --debug    turn on debug messages\n"
+						"  --verbose  turn on verbose debug messages\n"
+						"  --version  print application version and exit\n"
+				), MLPLAYER_APP_UNIX_NAME) << endl;
+				exit(EXIT_SUCCESS);
+			}
+			else if(arg == "--verbose")
+				verbose = true;
+			else if(arg == "--version")
+			{
+				QTextStream stream(stdout);
+				stream
+					<< QCoreApplication::applicationName()
+					<< " "
+					<< QCoreApplication::applicationVersion()
+					<< endl;
+				exit(EXIT_SUCCESS);
+			}
+			else
+				videos << arg;
+		}
+
+		if(debug)
+		{
+			if(verbose)
+				m::set_debug_level(m::DEBUG_LEVEL_VERBOSE);
+			else
+				m::set_debug_level(m::DEBUG_LEVEL_ENABLED);
+		}
+		else if(verbose)
+		{
+			MLIB_W(QApplication::tr(
+				"Invalid command line options: --verbose must be specified in conjunction with --debug." ));
+			exit(EXIT_FAILURE);
+		}
+	#if MLPLAYER_DEVELOP_MODE
+		else
+		{
+			m::set_debug_level(m::DEBUG_LEVEL_ENABLED);
+			//m::set_debug_level(m::DEBUG_LEVEL_VERBOSE);
+		}
+	#endif
+
+		return videos;
+	}
+
+
+}
+// TODO
+#if 0
+QString get_app_home_dir(void)
 {
-	using namespace mlplayer;
+	#ifdef Q_OS_UNIX
+		return QDir::home().filePath("." MLPLAYER_APP_UNIX_NAME);
+	#else
+		return QDir::home().filePath(MLPLAYER_APP_NAME);
+	#endif
+}
+#endif
 
-	QApplication app(argc, argv);
 
-	// Configuring application -->
-		QTextCodec::setCodecForCStrings(QTextCodec::codecForLocale());
 
-		app.setApplicationName(MLPLAYER_APP_NAME);
-#warning
-//		app.setApplicationVersion(get_version());
-//		APP_BINARY_PATH = QDir(app.applicationFilePath()).absolutePath();
-	// Configuring application <--
-
-	// Processing command line arguments
-//	process_command_line_options(app);
-//
-//	m::set_message_handler(m::MESSAGE_TYPE_INFO, &message_handler);
-//#if MLPLAYER_DEVELOP_MODE
-//	m::set_message_handler(m::MESSAGE_TYPE_SILENT_WARNING, &message_handler);
-//#endif
-//	m::set_message_handler(m::MESSAGE_TYPE_WARNING, &message_handler);
-//	m::set_message_handler(m::MESSAGE_TYPE_ERROR, &message_handler);
-		m::set_debug_level(m::DEBUG_LEVEL_VERBOSE);
-		m::set_debug_level(m::DEBUG_LEVEL_ENABLED);
+int main(int argc, char* argv[])
+{
+	std::auto_ptr<QApplication> app = m::gui::init(argc, argv, MLPLAYER_APP_NAME,
+		m::get_version(MLPLAYER_VERSION_MAJOR, MLPLAYER_VERSION_MINOR, MLPLAYER_VERSION_PATCH) );
 
 	MLIB_D("Starting the application...");
 
-	MLIB_A(argc == 2);
+	QString install_dir;
 
-	QString master_video_path = argv[1];
+	// Getting installation directory -->
+		try
+		{
+			install_dir = m::get_app_install_dir(MLPLAYER_APP_BIN_DIR);
+		}
+		catch(m::Exception& e)
+		{
+			MLIB_SW(QApplication::tr("Unable to determine application's installation directory"), EE(e));
+		}
+	// Getting installation directory <--
 
-	QList<QString> videos;
-	QList<medialib::Subtitles_info> subtitles;
-	medialib::get_tv_show_related_media(master_video_path, &videos, &subtitles);
+	// Loading translations
+	m::load_translations(
+		install_dir.isEmpty() ? install_dir : QDir(install_dir).absoluteFilePath(MLPLAYER_APP_TRANSLATIONS_DIR),
+		MLPLAYER_APP_UNIX_NAME
+	);
 
-Main_window* main_window = new Main_window(videos, subtitles, NULL);
-main_window->show();
-//media->enqueue("/home/username/music/song.mp3");
-//media->enqueue(":/sounds/endsound.ogg");
-//	Phonon::VideoWidget* player_widget = new Phonon::VideoWidget();
-//	Phonon::VideoPlayer* player = new Phonon::VideoPlayer();
-//	Phonon::MediaSource* source = new Phonon::MediaSource("/my_files/video/pub/What\ the\ Bleep\ Do\ We\ Know/What_the_Bleep_Do_We_Know.mkv");
-//	Phonon::createPath(source, player_widget);
-//	player.play();
+	// Handle operating system signals
+	m::sys::connect_end_work_system_signal(app.get(), SLOT(quit()));
 
-//	QString install_dir = get_install_dir();
-//	QLocale locale = QLocale::system();
-//
-//	QTranslator qt_translator;
-//	QTranslator app_translator;
-//	QTranslator mlib_translator;
-//
-//	// TODO: All code above does not show translated strings to the user.
-//
-//#ifdef Q_OS_UNIX
-//	// Qt's locale detection under UNIX works wrong -->
-//	{
-//		QMap<QString,QString> env_vars;
-//
-//		Q_FOREACH(const QString& var, QProcess::systemEnvironment())
-//		{
-//			int pos = var.indexOf('=');
-//			QString name = var.mid(0, pos);
-//			QString value = var.mid(pos + 1);
-//			env_vars[name] = value;
-//			MLIB_DV("Gotten an environment variable: '%1'='%2'.", name, value);
-//		}
-//
-//		if(!env_vars["LC_ALL"].isEmpty())
-//		{
-//			locale = QLocale(env_vars["LC_ALL"]);
-//			MLIB_D("Setting the locale to LC_ALL's value '%1'.", locale.name());
-//		}
-//		else if(!env_vars["LC_MESSAGES"].isEmpty())
-//		{
-//			locale = QLocale(env_vars["LC_MESSAGES"]);
-//			MLIB_D("Setting the locale to LC_MESSAGES's value '%1'.", locale.name());
-//		}
-//		else if(!env_vars["LANG"].isEmpty())
-//		{
-//			locale = QLocale(env_vars["LANG"]);
-//			MLIB_D("Setting the locale to LANG's value '%1'.", locale.name());
-//		}
-//	}
-//	// Qt's locale detection under UNIX works wrong <--
-//#endif
-//
-//	// Loading translations -->
-//	{
-//		bool is;
-//
-//		is = qt_translator.load("qt_" + locale.name(),
-//			QLibraryInfo::location(QLibraryInfo::TranslationsPath) );
-//		if(is)
-//			app.installTranslator(&qt_translator);
-//		else
-//			MLIB_D("Qt's translations for '%1' did not found.", locale.name());
-//
-//
-//		if(!install_dir.isEmpty())
-//		{
-//			is = app_translator.load(_F("%1_%2", MLPLAYER_APP_UNIX_NAME, locale.name()),
-//				QDir(install_dir).absoluteFilePath(MLPLAYER_APP_TRANSLATIONS_DIR) );
-//			if(is)
-//				app.installTranslator(&app_translator);
-//			else
-//				MLIB_D("Application's translations for '%1' did not found.", locale.name());
-//
-//			is = mlib_translator.load("mlib_" + locale.name(),
-//				QDir(install_dir).absoluteFilePath(MLPLAYER_APP_TRANSLATIONS_DIR) );
-//			if(is)
-//				app.installTranslator(&mlib_translator);
-//			else
-//				MLIB_D("MLib's translations for '%1' did not found.", locale.name());
-//		}
-//	}
-//	// Loading translations <--
-//
-//	// Handle operating system signals
-//	m::sys::connect_end_work_system_signal(&app, SLOT(quit()));
-//
-//	// Setting application icon -->
-//		if(!install_dir.isEmpty())
-//		{
-//			int sizes[] = { 8, 12, 16, 22, 24, 32, 48, 64, 128 };
-//
-//			QIcon icon;
-//
-//			BOOST_FOREACH(int size, sizes)
-//			{
-//				QString size_name = QString::number(size) + "x" + QString::number(size);
-//
-//				icon.addFile(
-//					QDir(install_dir).absoluteFilePath(
-//						MLPLAYER_APP_ICONS_DIR "/" + size_name + "/apps/" MLPLAYER_APP_UNIX_NAME ".png" ),
-//					QSize(size, size)
-//				);
-//			}
-//
-//			icon.addFile(
-//				QDir(install_dir).absoluteFilePath(
-//					MLPLAYER_APP_ICONS_DIR "/scalable/apps/" MLPLAYER_APP_UNIX_NAME ".svg" )
-//			);
-//
-//			app.setWindowIcon(icon);
-//		}
-//	// Setting application icon <--
-return app.exec();
+	// Setting application icon -->
+		if(!install_dir.isEmpty())
+		{
+			int sizes[] = { 24, 48 };
+
+			QIcon icon;
+
+			BOOST_FOREACH(int size, sizes)
+			{
+				QString size_name = QString::number(size) + "x" + QString::number(size);
+
+				icon.addFile(
+					QDir(install_dir).absoluteFilePath(
+						MLPLAYER_APP_ICONS_DIR "/" + size_name + "/apps/" MLPLAYER_APP_UNIX_NAME ".svg" ),
+					QSize(size, size)
+				);
+			}
+
+			icon.addFile(
+				QDir(install_dir).absoluteFilePath(
+					MLPLAYER_APP_ICONS_DIR "/48x48/apps/" MLPLAYER_APP_UNIX_NAME ".svg" )
+			);
+
+			app->setWindowIcon(icon);
+		}
+	// Setting application icon <--
+
+	try
+	{
+		// Processing command line arguments
+		QStringList args = process_command_line_options(app->arguments());
+
+		if(args.isEmpty())
+			M_THROW(QApplication::tr("There is no video file specified to play."));
+		else if(args.size() != 1)
+			M_THROW(QApplication::tr("Gotten too may video files to play. You must specify only one video file."));
+
+		QStringList videos;
+		QList<medialib::Subtitles_info> subtitles;
+
+		try
+		{
+			medialib::get_tv_show_related_media(args.first(), &videos, &subtitles);
+		}
+		catch(m::Exception& e)
+		{
+			M_THROW(PAM( _F(QApplication::tr("Error while getting '%1' info:"), args.first()), EE(e) ));
+		}
+
+		int exit_code;
+
+		// Running the application -->
+		{
+			mlplayer::Main_window main_window(videos, subtitles);
+			m::gui::set_main_window(&main_window);
+
+			main_window.show();
+			exit_code = app->exec();
+
+			MLIB_D("Destroying the main window...");
+			m::gui::set_main_window(NULL);
+		}
+		// Running the application <--
+
+		MLIB_D("Exiting with code %1...", exit_code);
+		return exit_code;
+	}
+	catch(m::Exception& e)
+	{
+		MLIB_W(_F( QApplication::tr("Unable to start %1"), MLPLAYER_APP_NAME ), EE(e) );
+		return EXIT_FAILURE;
+	}
 }
 
